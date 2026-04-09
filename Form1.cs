6232a96720ade9d8c05b56c338181869ffa57565
TAEYEON KIM <jenniferkim0704@outlook.kr>
2026-04-09 11:33:05 +0900

    과제1 코딩과 테스트 완료 주문하기 버튼을 누르면 주문내역과 총 금액 표시

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,52 @@ namespace BurgerKiosk
 
         private void btnOrder_Click(object sender, EventArgs e)
         {
+            int totalCost = 0;
 
+            // 햄버거 종류에 따른 가격 계산
+            if (rdoHamBurger.Checked)
+            {
+                totalCost += 5000;
+                lstOrder.Items.Add("햄버거 5,000원");
+            }
+
+            else if (rdoBulgogiBurger.Checked)
+            {
+                totalCost += 4000;
+                lstOrder.Items.Add("불고기버거 4,000원");
+            }
+
+            else if (rdoChickenBurger.Checked)
+            {
+                totalCost += 3000;
+                lstOrder.Items.Add("치킨버거 3,000원");
+            }
+
+            // 추가 메뉴에 따른 가격 계산
+            if (chkPotato.Checked)
+            {
+                totalCost += 3500;
+                lstOrder.Items.Add("감자튀김 3,500원");
+            }
+
+            if (chkCola.Checked)
+            {
+                totalCost += 2500;
+                lstOrder.Items.Add("콜라 2,500원");
+            }
+
+            if (chkCheese.Checked)
+            {
+                totalCost += 1500;
+                lstOrder.Items.Add("치즈 추가 1,500원");
+            }
+
+            if (chkSauce.Checked)
+            {
+                totalCost += 500;
+                lstOrder.Items.Add("소스 추가 500원");
+            }
+            lblTotalCost.Text = "총 금액:" + totalCost + "원";
         }
 
         private void btnInit_Click(object sender, EventArgs e)
